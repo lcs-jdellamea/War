0a9e8fdc98ffd59bc6d52fa871edf3aeef2efb92
Jack Dellamea <jdellamea@lcs.on.ca>
2023-04-20 09:14:28 -0400

    Initial Commit

---
+++ War/WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarApp.swift
+//  War
+//
+//  Created by Jack Dellamea on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
